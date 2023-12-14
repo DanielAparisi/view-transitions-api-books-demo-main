d37a2e77570cc2165557127608421239bfb92fe6
Daniel Aparisi <daniel.aparisi.lozano.com>
2023-12-14 23:53:42 +0100

    init Commit

---
+++ src/data/books.js
@@ -0,0 +1,31 @@
+export const books = [
+  {
+      id: 'Almacenaje',
+      title: 'Proceso de almacenaje',
+      img:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Mercadona_em_Canidelo.jpg/330px-Mercadona_em_Canidelo.jpg',
+      opinion: 'Un clásico entre los clásicos del mundo de la programación. Aunque en pleno 2023 yo tengo una opinión un poco polémica. No me parece un libro que recomendaría a todo el mundo, especialmente a aquellas personas que están aprendiendo. Además, tiene un tono muy dogmático y existe gente que se lo toma, peligrosamente, como la biblia del desarrollo. Pero si eres capaz de leerlo tomando perspectiva, hay cosas interesantes que todavía están vigentes. Ojo que todos los ejemplos de código están basados en Java y mucha programación orientada a objetos.',
+      author: 'Daniel Aparisi',
+  },
+  {
+      id: 'Almacen',
+      title: 'Proceso de Almacenamiento',
+      img:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Mercadona_em_Canidelo.jpg/330px-Mercadona_em_Canidelo.jpg',
+      opinion: 'Cuando la mercancía entra en el almacén, un ordenador la identifica (se mide, pesa y se comprueba que corresponde con la etiqueta que identifica el producto). Después, se coloca en los llamados palets esclavos que cuentan con un microchip que facilita su seguimiento por todo el centro. En Ciempozuelos, se pueden llegar a almacenar hasta 54.000 palets en diferentes temperaturas: ambiente, pero controlada; a 12 grados (para fruta, verdura, charcutería y chocolates), a 3 grados (carne y productos refrigerados) y a menos 23 grados (congelados). Ésta se mantendrá durante todo el proceso hasta su colocación en la estantería de la tienda.         Los productos de gran rotación como el papel higiénico o el agua no se manipulan y el palet va directamente a la tienda. Este tipo de  mercancías  se  almacenan   en  40 pasillos de 12 metros de altura y  son   desplazados   mediante   ascensores   automáticos  a una velocidad de  4  metros por  segundo.   Estos   ascensores   entregan   y   recogen   mercancías   en  cada  viaje.  De media,  un   palet   permanece  en el  almacén   dos   días.   En  caso de  avería, los productos se  distribuyen  por todos los pasillos y  estantes   para   evitar   interrupciones   en   las   operaciones   del   almacén.  El proceso  de   preparación  para  un   puesto  de  baja  rotación  es más complejo. Mercadona ha  desarrollado  un  almacén  propio  adicional  para estos  productos  a base de cajas y bandejas de plástico  con  espacio   para  800.000  piezas.  El primer paso es  colocar   el   producto   en   la   bandeja   donde   se   almacenará.   Pero   primero   tienes   que   retirar  la mercancía  de   los   palés   enviados   por   tu   proveedor.   Este   proceso  se  realiza  mediante   un   despaletizador,  una máquina que  puede  separar  las   capas   del   producto  una a una  gracias   a   la   absorción  de  aire.Luego   deberás   seleccionar  una caja o  bandeja   para   cada   producto.  Varias cintas transportadoras facilitan  las   bodas  y  los   productos   se   pueden   colocar  en  bandejas  con  chips   de  seguimiento.  Las   tiendas   ahora   pueden   almacenarlos  hasta que  haya   demanda.  Esta  parte  del proceso  fue  una de las más difíciles para  Mercadona   porque   hubo   que   configurar  el sistema para que  reconociera  diversos formatos.',
+      author: 'Teresa Ramon',
+  },
+  {
+      id: 'Clasificacion',
+      title: 'Proceso de Clasificacion de los productos',
+      img:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Mercadona_em_Canidelo.jpg/330px-Mercadona_em_Canidelo.jpg',
+      opinion: 'Tienen un almacén automatizado, las máquinas son las q reciben y organizan la mercancía.Llegan los productos, el transportista los descarga en las máquinas y a partir d ahí es un ordenador el q se encarga d comprobar cantidades, posibles fallos...El resultado es ahorro d esfuerzo y tiempo',
+      author: 'Marcos Dominguez',
+  },
+  {
+      id: 'Control',
+      title: 'Proceso de Control de los productos',
+      img:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Mercadona_em_Canidelo.jpg/330px-Mercadona_em_Canidelo.jpg',
+      opinion: 'En cuanto a la gestion de sus recursos, Mercadona utiliza un sistema ERP (',
+      author: 'Sergio Rodriguez',
+  },
+
+]
